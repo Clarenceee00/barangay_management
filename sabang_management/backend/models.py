a2636003f0bf8f64fb3cd1544c861b03f20cb5ec
Clarenceee00 <simbulanclarence30@gmail.com>
2025-03-18 15:35:56 +0800

    user/admin

--- sabang_management/backend/models.py
+++ sabang_management/backend/models.py
@@ -1,12 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-class Users(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    email = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=50)
+class Admin(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
 
-    def __str__(self):
-        return self.username
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            username,
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class BarangayClearance(models.Model):
